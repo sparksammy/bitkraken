d3cc893c6045196e032a5f4df2ffbc251b91832d
Samuel Lord <sllord@icloud.com>
2020-12-02 23:42:24 -0600

    Quick update before bed... :-)
    Auto-add private keys lmao

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
+bitcoin/bin/bitcoind -daemon
 KEYBASE=$(shuf -n 1 NotDansSecLists.txt)
-echo "$KEYBASE = "
-printf $KEYBASE | sha256sum
+KEY=$(echo $KEYBASE | sha256sum)
+KEY=$(echo $KEY | cut -c 1-64)
+printf "\n$KEYBASE = "
+printf "$KEY\n"
+bitcoin/bin/bitcoin-cli importprivkey "$KEY" "$KEYBASE$RANDOM" # imports key to unencrypted wallet
+sleep .7 # important so things don't crash as easily. (i hope.)
 ./run.sh
